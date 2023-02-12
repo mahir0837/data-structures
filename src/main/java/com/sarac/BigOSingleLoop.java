63afe57c6d37e7d0f5f764298b123df8d6aca635
owner <mahirsarac89@gmail.com>
2023-02-11 20:08:58 -0500

    BigOSingleLoop class created

---
+++ src/main/java/com/sarac/BigOSingleLoop.java
@@ -0,0 +1,40 @@
+package com.sarac;
+
+public class BigOSingleLoop {
+    public static void main(String[] args) {
+        long numberOfOperations=0;
+        int n=100;// size of  Data
+
+        long startTime;
+        long endTime;
+        System.out.println("Input size n is = "+ n);
+
+        // Task 1 Single Loop................... O(n)
+        startTime=System.currentTimeMillis();
+        for(int i=0;i<n;i++){
+            numberOfOperations+=1;
+            endTime=System.currentTimeMillis();
+        }
+
+        // Task 2 Nested Loops...................
+
+        // Task 2 End...................
+// ......................................................................................................
+        // Task 3 Three Nested Loops...................
+
+
+        // Task 3 End...................
+// ......................................................................................................
+        // Task 4 N and M Nested Loops  O(n*m)...................
+
+        // Task 4 End...................
+
+        // Task 4.5 N and M Consecutive Loops O(n+m)...................
+
+        // Task 4.5 End...................
+
+        // Task 5 Logarithmic Complexity...................
+
+       // Task 5 End...................*/
+    }
+}
